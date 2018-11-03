ccce1f6450f742e3e512a56a18eb1dff2f23f1c8
Alyssa Oda <alyoda@umich.edu>
2018-11-03 13:24:58 -0400

    change setup for react

---
+++ votenow/js/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import VotePage from './voterinfo';
+
+ReactDOM.render(
+  <VotePage url="" />,
+  document.getElementById('reactEntry'),
+);
---
+++ votenow/js/voterinfo.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import fetch from 'isomorphic-fetch';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+class HomePage extends React.Component {
+  constructor(props) {
+    // Initialize mutable state
+    super(props);
+
+    this.state = {};
+
+  }
+
+  showVehicle(e) {
+
+  }
+
+
+  handlChange(e) {
+    
+  }
+
+  executeSearch() {
+
+  }
+
+
+  render() {
+    return(<p>Hello World!</p>);
+  }
+}
+
+export default HomePage;
