bce6db634cb17773171dd3341ed4edd16b796827
Adam Stautberg <Adam@Adams-MacBook-Pro-2.local>
2018-11-05 09:00:32 -0500

    rendering the vote info, added function to loop through all races and candidates and render the info

--- votenow/js/voteinfo.jsx
+++ votenow/js/voteinfo.jsx
@@ -7,8 +7,13 @@ class VoteInfo extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
-
+    this.state = {
+	          open: false,
+		  contests: [{candidates: [{}]}],
+		  election: {},
+		  pollingLocations: [{address: {}}],
+		  state: [],
+    };
   }
 
   componentDidMount() {
@@ -19,15 +24,54 @@ class VoteInfo extends React.Component {
       })
       .then((data) => {
         this.setState(data);
-        console.log(data);
       })
       .catch((error) => console.log(error));
   }
 
+  createRaces(contests: [{candidate: []}]) {
+    let Races = []
+
+    // Outer loop to create parent
+    for (let i = 0; i < contests.length; i++) {
+      if(contests[i].office == undefined){
+	return Races
+      }
+      Races.push(<h4> {contests[i].office}: </h4>);
+      Races.push(<p> Candidates:</p>);
+      for (let j = 0; j < contests[i].candidates.length; j++) {
+        Races.push(<p> {contests[i].candidates[j].party}:  
+		{contests[i].candidates[j].name}</p>)
+      }
+    }
+    return Races
+  }
+
 
   render() {
-    console.log(this.props.url);
-    return(<p>Hello World!</p>);
+    console.log(this.state);
+    var address = {}
+    address = this.state.pollingLocations[0].address;
+    var contests = []
+    contests = this.state.contests;
+    console.log(address.locationName);
+		
+    return(
+	<div>
+	<h2> Based on your address: </h2>
+	<h4> Your nearest polling station is: </h4>
+	<p>{address.locationName}<br /> {address.line1} <br />
+	{address.city}, {address.state}, {address.zip}</p>
+
+	<h4> When you can vote: </h4>
+	<p> {this.state.pollingLocations[0].pollingHours} </p>
+
+	<h3> The local election Races include: </h3>
+	{this.createRaces(contests)}
+	</div>
+
+    );
+
+	
   }
 }
 
